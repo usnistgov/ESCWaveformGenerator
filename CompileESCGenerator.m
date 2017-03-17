82bdcaa74a44c0c6c579eb848e5e09ac7ebe7b08
Caromi <rmc1@NIST.GOV>
2017-03-17 13:43:14 -0400

    added splash screen

--- CompileESCGenerator.m
+++ CompileESCGenerator.m
@@ -19,8 +19,8 @@ APP_SOURCE_PATH=fullfile(ROOT_DIR,SOURCE_DIR,IN_APP_DIR,APP_SOURCE_NAME);
 
 
 OUT_DIR=fullfile(ROOT_DIR,APP_DIR);
-ICON_DIR='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_icon.ico';
-
+ICON_FILE='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_icon.ico';
+SPLASH_FILE='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_splash.png';
 %EXT_DIR='ext';
 
 SEARCH_DIR1=fullfile(SOURCE_DIR,IN_APP_DIR); % start from root
@@ -31,8 +31,9 @@ FUNC1=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME1);
 FUNC2=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME2);
 %SEARCH_DIR3=fullfile(SOURCE_DIR,EXT_DIR);
 
-mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-a',FUNC1 ,'-a', FUNC2,'-r',ICON_DIR);
+mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-a',FUNC1 ,'-a', FUNC2,'-r',ICON_FILE);
 
 cd(SOURCE_DIR);
 copyfile config ..\bin\config;
+copyfile(SPLASH_FILE, '..\bin\app\splash.png')
 cd('..')
