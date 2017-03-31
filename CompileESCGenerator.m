66b0ce965d58664bc2b8ec669a25d75a8fd4ee16
Caromi <rmc1@NIST.GOV>
2017-03-31 11:39:16 -0400

    Update compile options

--- CompileESCGenerator.m
+++ CompileESCGenerator.m
@@ -5,7 +5,8 @@ SOURCE_DIR='src';
 BUILD_DIR='bin';
 APP_DIR='bin\app';
 IN_APP_DIR='app';
-UTIL_DIR='util';
+DSP_DIR='dsp';
+%UTIL_DIR='util';
 if exist(BUILD_DIR,'dir')~=7
     mkdir(BUILD_DIR)
 end
@@ -25,13 +26,14 @@ SPLASH_FILE='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_s
 
 SEARCH_DIR1=fullfile(SOURCE_DIR,IN_APP_DIR); % start from root
 %FUNC_DIR=fullfile(SOURCE_DIR, UTIL_DIR); % start from root
-FUNC_NAME1='read_xsl_info_f.m';
-FUNC_NAME2='radar_data_reader.m';
-FUNC1=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME1);
-FUNC2=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME2);
-%SEARCH_DIR3=fullfile(SOURCE_DIR,EXT_DIR);
+%FUNC_NAME1='read_xsl_info_f.m';
+%FUNC_NAME2='radar_data_reader.m';
+%FUNC1=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME1);
+%FUNC2=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME2);
+SEARCH_DIR=fullfile(SOURCE_DIR,DSP_DIR);
 
-mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-a',FUNC1 ,'-a', FUNC2,'-r',ICON_FILE);
+%mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-a',FUNC1 ,'-a', FUNC2,'-r',ICON_FILE);
+mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-I',SEARCH_DIR,'-r',ICON_FILE);
 
 cd(SOURCE_DIR);
 copyfile config ..\bin\config;
