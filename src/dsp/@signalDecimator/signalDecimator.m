a2c3fe4223bcfd6a95d7563390103bbb4f397f33
Caromi <rmc1@NIST.GOV>
2017-10-11 14:32:55 -0400

    Update decimation code and comments

--- src/dsp/@signalDecimator/signalDecimator.m
+++ src/dsp/@signalDecimator/signalDecimator.m
@@ -1,6 +1,16 @@
 classdef signalDecimator<radarSignalFromFile & signalToFile
     %signal decimator class
-    %   read radar signal, decimate and save to a file
+    %read radar waveform file, decimate and save it to a file
+    %Example:
+    %       filterSpec.filtCoef=.....; % anti-aliasing filter coefficients vector
+    %       %Construct a signal decimator with appropriate parameters
+    %       signalDec=signalDecimator(inputFile,outputFile,oldFs,newFs,freqShift,filterSpec);
+    %       %Intitiate signal decimator
+    %       signalDec=initDecimator(signalDec);
+    %       %Start decimating the file
+    %       signalDec=decimateFile(signalDec);
+    %       %Close input and output files
+    %       signalDec=resetSignalDecimator(signalDec);
     %see also radarSignalFromFile, signalToFile
     
     properties
@@ -23,12 +33,14 @@ classdef signalDecimator<radarSignalFromFile & signalToFile
             this.freqShift=freqShift;
             this.filterSpec=filterSpec;
         end
-        function this=initDecimator(this,initialSeekSamples,samplesPerSegment)
-%            this=setMeasFile(this);
+        function this=initDecimator(this)
+            %I&Q were swapped in the orignal NASCTN waveform files 
             this.inputIQDirection='QI';
+            %Switch output files to I&Q
             this.outputIQDirection='IQ';
             this.EOFAction='Rnormal';
-            this.samplesPerSegment=samplesPerSegment;
+            %this.samplesPerSegment=samplesPerSegment;
+            initialSeekSamples=0;
             this=setSeekPositionSamples(this,initialSeekSamples);
             this=setReadScale(this,1);
             this=initInputFile(this);
@@ -48,7 +60,7 @@ classdef signalDecimator<radarSignalFromFile & signalToFile
                  if leftOverSamples>0
                      numOfSegmentsWithLeftOver=numOfSegments+1;
                  end
-                 filterRESET=0;
+                 filterRESET=false;
                  t0=0;
                  for I=1:numOfSegmentsWithLeftOver
                      if (I==numOfSegmentsWithLeftOver) && (numOfSegmentsWithLeftOver~=numOfSegments)
@@ -57,7 +69,7 @@ classdef signalDecimator<radarSignalFromFile & signalToFile
                      sigMeas =readSamples(this);
                      t=((0:this.samplesPerSegment-1).')*(1/this.oldFs)+t0;
                      this=seekNextPositionSamples(this);
-                     sigMeasShifted=sigMeas.*exp(1i*2*pi*(this.freqShift)*t);
+                     sigMeasShifted=sigMeas.*exp(-1i*2*pi*(this.freqShift)*t);
                      [sigResampled,~]=dspFun.resampleFilt(sigMeasShifted,this.oldFs,this.newFs,filterRESET,this.filterSpec);
                      writeSamples(this,sigResampled);
                      t0=t(end)+1/this.oldFs;
