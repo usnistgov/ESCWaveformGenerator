1e5c48ec414568285d363138411d7c803a1d6a45
Caromi <rmc1@NIST.GOV>
2017-11-01 13:59:25 -0400

    update compile code

--- CompileESCGenerator.m
+++ CompileESCGenerator.m
@@ -6,6 +6,18 @@
 ...% You are solely responsible for determining the appropriateness of using and distributing the software and you assume all risks associated with its use, including but not limited to the risks and costs of program errors, compliance with applicable laws, damage to or loss of data, programs or equipment, and the unavailability or interruption of operation. This software is not intended to be used in any situation where a failure could cause risk of injury or damage to property. The software developed by NIST employees is not subject to copyright protection within the United States.
 
 %%
+%Requires Toolboxes:
+%                     'MATLAB'
+%                     'Signal Processing Toolbox'
+%                     'DSP System Toolbox'
+%                     'Communications System Toolbox'
+%                     'Parallel Computing Toolbox'
+%                     'MATLAB Distributed Computing Server'
+%Additional Toolboxes:
+%                     'LTE System Toolbox' --> for LTE file Generation
+%                     'MATLAB Compiler' --> for compiling 
+% All toolboxes are needed for compilation 
+
 ROOT_DIR=pwd;
 SOURCE_DIR='src';
 BUILD_DIR='bin';
@@ -14,7 +26,10 @@ IN_APP_DIR='app';
 DSP_DIR='dsp';
 UTIL_DIR='util';
 RES_DIR='res';
-ICON_FILE='ESCGenIconVc.res';
+
+ICON_FILE='icon.ico';
+SPLASH_FILE='splash.png';
+
 if exist(BUILD_DIR,'dir')~=7
     mkdir(BUILD_DIR)
 end
@@ -30,26 +45,22 @@ APP_SOURCE_PATH=fullfile(ROOT_DIR,SOURCE_DIR,IN_APP_DIR,APP_SOURCE_NAME);
 OUT_DIR=fullfile(ROOT_DIR,APP_DIR);
 %ICON_FILE='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_icon.ico';
 ICON_PATH=fullfile(ROOT_DIR,SOURCE_DIR,RES_DIR,ICON_FILE);
-SPLASH_FILE='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_splash.png';
-%EXT_DIR='ext';
+%SPLASH_PATH='C:\Program Files\MATLAB\R2016b\toolbox\compiler\Resources\default_splash.png';
+SPLASH_FILE=fullfile(ROOT_DIR,SOURCE_DIR,RES_DIR,SPLASH_FILE);
 
-%SEARCH_DIR1=fullfile(SOURCE_DIR,IN_APP_DIR); % start from root
-%FUNC_DIR=fullfile(SOURCE_DIR, UTIL_DIR); % start from root
-%FUNC_NAME1='read_xsl_info_f.m';
-%FUNC_NAME2='radar_data_reader.m';
-%FUNC1=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME1);
-%FUNC2=fullfile(ROOT_DIR,SOURCE_DIR, UTIL_DIR,FUNC_NAME2);
 SEARCH_DIR1=fullfile(SOURCE_DIR,DSP_DIR);
 SEARCH_DIR2=fullfile(SOURCE_DIR,UTIL_DIR);
-%mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-a',FUNC1 ,'-a', FUNC2,'-r',ICON_FILE);
 mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d', OUT_DIR,'-v',APP_SOURCE_PATH,'-I',SEARCH_DIR1,'-I',SEARCH_DIR2,'-r',ICON_PATH);
+%mcc('-o', APP_NAME,'-W',WinMain,'-T', 'link:exe' ,'-d',OUT_DIR,'-v',APP_SOURCE_PATH,'-I',SEARCH_DIR1,'-I',SEARCH_DIR2,'-r',ICON_PATH);
 
-% cd(SOURCE_DIR);
-% copyfile config ..\bin\config;
-% copyfile res ..\bin\res;
-% copyfile(SPLASH_FILE, '..\bin\app\splash.png')
-% cd('..')
+cd(SOURCE_DIR);
+copyfile config ..\bin\config;
+copyfile res\CTL_logo.png ..\bin\res\CTL_logo.png;
+copyfile(SPLASH_FILE, '..\bin\app\splash.png')
+cd('..')
 
 %% restart matlab
-!matlab &
-exit
+% !matlab &
+% exit
+
+%mcc -o ESCWaveformGenerator -W WinMain:ESCWaveformGenerator -T link:exe -d 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\ESCWaveformGenerator\for_testing' -v 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\app\ESCWaveformGenerator.mlapp' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@dspFun\dspFun.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@executor\executor.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\util\@folderBrowserGUI\folderBrowserGUI.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@radarSignalFromFile\radarSignalFromFile.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@signalFromFile\signalFromFile.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@signalToFile\signalToFile.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@threeGPPChannel\threeGPPChannel.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\util\@utilFun\utilFun.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@waveform\waveform.m' -a 'D:\Spectrum-Share\Waveforms data\ESCWaveformGenerator\src\dsp\@waveformGenerator\waveformGenerator.m' -r 'C:\Program Files\MATLAB\R2017b\toolbox\compiler\Resources\default_icon.ico' 
